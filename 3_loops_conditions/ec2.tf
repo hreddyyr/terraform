fba60967f4b7e481ec09722cf890e5b6aa72d772
hreddyyr <hreddyyr@gmail.com>
2024-10-03 12:55:25 +0530

    output

--- 3_loops_conditions/ec2.tf
+++ 3_loops_conditions/ec2.tf
@@ -1,7 +1,8 @@
 resource "aws_instance" "web" {
-  count = 11
+  #count = 11
+  count = length(var.instance_names)
   ami           = var.ami
-  instance_type = var.instance_names[count.index] == "mongodb" ? "t3.small" : "t2.micro"
+  instance_type = var.instance_names[count.index] == "mongodb" || var.instance_names[count.index] == "my_sql" || var.instance_names[count.index] == "shipping" ? "t3.small" : "t2.micro"
   vpc_security_group_ids = [aws_security_group.roboshop_all.id]
 
   tags = {
